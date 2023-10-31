8beebbf1aa084c2c9e208bdc5e127a0a31413dff
MiKami12345 <sakaki299@gmail.com>
2023-11-01 02:27:01 +0900

    展開と縮小

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,18 +1,41 @@
+// htmlファイルを完全に読み込んだ後に実行
+document.addEventListener('DOMContentLoaded', function() {
 
-window.addEventListener('scroll', ScrollTop);
+    //画面右下のボタン、画面上部に戻る
+    window.addEventListener('scroll', ScrollTop);
 
-function scrollToTop() {
-    window.scroll({
-        top: 0,
-        behavior: 'smooth'
-    });
-}
-
-function ScrollTop() {
-    const button = document.querySelector('.scroll-to-top');
-    if (document.documentElement.scrollTop > 100) {
-        button.style.display = 'block';
-    } else {
-        button.style.display = 'none';
+    function scrollToTop() {
+        window.scroll({
+            top: 0,
+            behavior: 'smooth'
+        });
     }
-}
+
+    function ScrollTop() {
+        const button = document.querySelector('.scroll-to-top');
+        if (document.documentElement.scrollTop > 100) {
+            button.style.display = 'block';
+        } else {
+            button.style.display = 'none';
+        }
+    }
+
+    // projectsで表示する
+    const projectItems = document.querySelectorAll('.project_item');
+
+    projectItems.forEach((project, index) => {
+        const moreButton = project.querySelector('.icon_more');
+        const projectContent = project.querySelector('.project_content');
+    
+        moreButton.addEventListener('click', () => {
+            if (projectContent.style.display === 'none') {
+                projectContent.style.display = 'block';
+                moreButton.classList.add('is_open');
+            } else {
+                projectContent.style.display = 'none';
+                moreButton.classList.remove('is_open');
+            }
+        });
+    });
+
+});
